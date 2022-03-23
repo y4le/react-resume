f307d54541393937623e34d157b4d8bcc3f52eaf
Yale Thomas <lordchair@gmail.com>
2022-03-23 08:30:56 -0700

    server side render pdf

---
+++ src/pdf/pdfRenderer.mjs
@@ -0,0 +1,11 @@
+/*
+ * Supply PdfResume with informaiton to display and render it to a file.
+ */
+
+import React from 'react';
+import ReactPDF from '@react-pdf/renderer';
+
+import PdfResume from './PdfResume';
+import content from '../js/content';
+
+ReactPDF.render(<PdfResume content={content} />, `${__dirname}/../../dist/yale_resume.pdf`);
